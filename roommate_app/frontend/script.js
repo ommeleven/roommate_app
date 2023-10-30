ef08b43c039adc79a826e509212f67b3f293535a
ommeleven <dangeom94@gmail.com>
2023-10-29 22:41:00 -0500

    backend setup

--- roommate_app/frontend/script.js
+++ roommate_app/frontend/script.js
@@ -1,8 +1,7 @@
 fetch('https://your-api-url/rooms')
   .then(response => response.json())
   .then(data => {
-    // Process the data and update your frontend
-    console.log(data); // You may want to update the UI with this data
+    console.log(data); 
   })
   .catch(error => {
     console.error('Error:', error);
