af8e0aee5f0a0cf2e762abda3213826ac3124de7
ommeleven <dangeom94@gmail.com>
2023-10-31 17:53:42 -0500

    backend: user auth login fixed

--- roommate_app/backend/user_auth.py
+++ roommate_app/backend/user_auth.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from roommate_app.backend.models import UserProfile
 
 def register_user(username, email, password):
     user = User.objects.create_user(username, email, password)
