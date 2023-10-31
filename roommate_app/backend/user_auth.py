b9a2e90b93ebdea713463ce0f3a13421be72ab87
ommeleven <dangeom94@gmail.com>
2023-10-31 17:51:05 -0500

    backend: apartment class

---
+++ roommate_app/backend/user_auth.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+
+def register_user(username, email, password):
+    user = User.objects.create_user(username, email, password)
+    user.save()
+
+def login_user(username, password):
+    user = User.objects.authenticate(username, password)
+    if user is not None:
+        login(user)
+        return True
+    else:
+        return False
+
+def get_user_profile(user):
+    profile = UserProfile.objects.get(user=user)
+    return profile
+
+def update_user_profile(user, profile):
+    profile.user = user
+    profile.save()
