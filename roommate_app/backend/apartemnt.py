eaf5a3dd65ffdba9da6b5469ccde92a07bcc5b23
ommeleven <dangeom94@gmail.com>
2023-11-02 00:01:32 -0500

    restructured apartment.py

--- roommate_app/backend/apartemnt.py
+++ roommate_app/backend/apartemnt.py
@@ -1,53 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class ApartmentManager(models.Manager):
+    def create_apartment(self, user, name, location):
+        return self.create(user=user, name=name, location=location)
+
 class Apartment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
 
+    objects = ApartmentManager()
+
     def __str__(self):
         return self.name
-    
-    def create_apartment(user, name, location):
-        apartment = Apartment.objects.create(user=user, name=name, location=location)
-        apartment.save()
 
-    def get_user_apartments(user):
-        apartments = Apartment.objects.filter(user=user)
-        return apartments
-
-    def update_apartment(apartment, name, location):
-        apartment.name = name
-        apartment.location = location
-        apartment.save()
-
-    def delete_apartment(apartment):
-        apartment.delete()
+class RoomManager(models.Manager):
+    def create_room(self, apartment, room_type, name):
+        return self.create(apartment=apartment, room_type=room_type, name=name)
 
 class Room(models.Model):
     apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE)
     room_type = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
 
+    objects = RoomManager()
+
     def __str__(self):
         return self.name
 
-    def create_room(apartment, room_type, name):
-        room = Room.objects.create(apartment=apartment, room_type=room_type, name=name)
-        room.save()
-
-    def get_apartment_rooms(apartment):
-        rooms = Room.objects.filter(apartment=apartment)
-        return rooms
-
-    def update_room(room, room_type, name):
-        room.room_type = room_type
-        room.name = name
-        room.save()
-
-    def delete_room(room):
-        room.delete()
+class ReservationManager(models.Manager):
+    def create_reservation(self, room, user, start_date, end_date):
+        return self.create(room=room, user=user, start_date=start_date, end_date=end_date)
 
 class Reservation(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
@@ -55,22 +39,7 @@ class Reservation(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
 
+    objects = ReservationManager()
+
     def __str__(self):
         return self.room.name
-
-    def create_reservation(room, user, start_date, end_date):
-        reservation = Reservation.objects.create(room=room, user=user, start_date=start_date, end_date=end_date)
-        reservation.save()
-
-    def get_user_reservations(user):
-        reservations = Reservation.objects.filter(user=user)
-        return reservations
-
-    def update_reservation(reservation, start_date, end_date):
-        reservation.start_date = start_date
-        reservation.end_date = end_date
-        reservation.save()
-
-    def delete_reservation(reservation):
-        reservation.delete()
-
